e3a05ed251def1a22f165b2e1c6d7898de5690cc
Howard Shaw <68131536+howardthegr8one@users.noreply.github.com>
2024-01-18 13:16:08 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,19 +1,3 @@
 function binarySearch(list, element) {
-    // checks for empty or invalid lists
-    if (list.length < 1 || element > list[list.length-1])
-        return -1
-    if (list.length == 1)
-        (list.length == 1) ? 0 : -1
-        
-    let mid = Math.floor(list.length / 2)
-
-    if (element <= list[mid])
-        if (element == list[mid-1])
-            return binarySearch(list.slice(0, mid), element)
-        else if (element <= list[mid-1])
-            return binarySearch(list.slice(0, mid), element)
-        else
-            return mid
-    else
-        return mid + binarySearch(list.slice(mid, list.length), element)
+    return -1;
 }
